cc02b32bae82442dd0ededc1513dbb5341875401
Cassie Jeng <59851290+cjeng8771@users.noreply.github.com>
2023-02-16 04:32:49 -0600

    experiment 2-16-23 with data

---
+++ experiment_2-16-23/jeng_filetransfer.sh
@@ -0,0 +1,5 @@
+
+for HOST in jengc@pc05-fort.emulab.net jengc@pc15-fort.emulab.net jengc@pc12-fort.emulab.net jengc@pc11-fort.emulab.net jengc@pc09-fort.emulab.net jengc@pc10-fort.emulab.net jengc@pc07-fort.emulab.net jengc@pc16-fort.emulab.net
+do
+    scp QPSK_signal_2023_01_18_neal.iq $HOST:
+done
